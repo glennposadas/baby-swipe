c3680fe6e41e952b0fe89261fa60b6ab9fbd58b2
Glenn Posadas <hello@glennvon.com>
2023-08-21 03:28:47 +0800

    add base proj.

---
+++ BabiesFirsts/BabiesFirstsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabiesFirstsApp.swift
+//  BabiesFirsts
+//
+//  Created by Glenn Posadas on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BabiesFirstsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
---
+++ BabiesFirsts/WelcomeView.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeView.swift
+//  BabiesFirsts
+//
+//  Created by Glenn Posadas on 8/16/23.
+//
+
+import Lottie
+import SwiftUI
+
+struct WelcomeView: View {
+  var body: some View {
+    LottieView(animation: .named("hello"))
+      .looping()
+      .frame(maxWidth: 100)
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    WelcomeView()
+  }
+}
