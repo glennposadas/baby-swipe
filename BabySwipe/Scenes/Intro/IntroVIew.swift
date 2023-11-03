917af8a57b18d0de494f98f964685fa402feb7dd
Glenn Posadas <hello@glennvon.com>
2023-11-03 10:45:58 +0800

    wip

--- BabySwipe/Scenes/Intro/IntroVIew.swift
+++ BabySwipe/Scenes/Intro/IntroVIew.swift
@@ -14,7 +14,6 @@ struct IntroVIew: View {
   // MARK: Properties
   
   @State private var isAnimating = false
-  @State private var isAnimatingWalking = false
   
   // MARK: -
   // MARK: Functions
@@ -68,6 +67,23 @@ struct IntroVIew: View {
     }
     .edgesIgnoringSafeArea(.all)
   }
+}
+
+struct AnimatedCharacterView: View {
+  
+  @State var index: Int
+  @State private var isAnimating = false
+  
+  var body: some View {
+    LottieView(animation: .named("char\(index)"))
+        .looping()
+        .frame(width: 250)
+        .offset(
+          x: isAnimating ? targetEndXPt(index: index)
+          : initialXPt(index: index),
+            y: UIScreen.main.bounds.height * 0.15
+        )
+  }
   
   private func initialXPt(index: Int) -> CGFloat {
     return (UIScreen.main.bounds.width - 460) - CGFloat(index * 100) - 200
@@ -75,12 +91,6 @@ struct IntroVIew: View {
   
   private func targetEndXPt(index: Int) -> CGFloat {
     return CGFloat(index * 100) + (UIScreen.main.bounds.width * 0.8)
-  }
-}
-
-struct AnimatedCharacterView: View {
-  var body: some View {
-    
   }
 }
 
