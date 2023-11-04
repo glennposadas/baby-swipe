c6fa1c439ef932f25b0d5d67aa531541788f3d4d
Glenn Posadas <hello@glennvon.com>
2023-11-04 10:25:15 +0800

    fix layouts, disable split screen.

---
+++ BabySwipe/Scenes/Home/Cell.swift
@@ -0,0 +1,22 @@
+//
+//  Cell.swift
+//  BabyVision
+//
+//  Created by Glenn Posadas on 10/16/23.
+//
+
+import SwiftUI
+
+struct Cell: View {
+  
+  @State var category: Category
+  
+  var body: some View {
+    category.icon
+      .resizable()
+      .scaledToFill()
+      .frame(minWidth: 0, maxWidth: .infinity)
+      .cornerRadius(10)
+      .shadow(color: Color.primary.opacity(0.3), radius: 1)
+  }
+}
