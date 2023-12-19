61dbb8840dfcb235bf5d429b5f106e6ca542729e
Glenn Posadas <hello@glennvon.com>
2023-12-20 00:14:02 +0800

    more ipad fixes.

--- BabySwipe/Scenes/Game/GameView.swift
+++ BabySwipe/Scenes/Game/GameView.swift
@@ -86,10 +86,6 @@ struct GameView: View {
           }
           .padding(.bottom, 50)
           
-          Rectangle()
-            .frame(height: 100)
-            .foregroundColor(Color.red)
-          
           Spacer()
         }
       }
