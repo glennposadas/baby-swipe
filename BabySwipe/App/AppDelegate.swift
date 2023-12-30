9bad011eccc57bc0748fc541480299c80fb8d22e
Glenn Posadas <hello@glennvon.com>
2023-12-30 11:24:16 +0800

    wip.

--- BabySwipe/App/AppDelegate.swift
+++ BabySwipe/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import Firebase
 import GoogleMobileAds
+import RevenueCat
 import SwiftUI
 
 var isIpad = UIDevice.current.userInterfaceIdiom == .pad
@@ -14,8 +15,15 @@ var isIpad = UIDevice.current.userInterfaceIdiom == .pad
 class AppDelegate: NSObject, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
     FirebaseApp.configure()
+    
     GADMobileAds.sharedInstance().start(completionHandler: nil)
+    
+    let ass = AppKeys.revCatAPIKey
+    
+    Purchases.logLevel = .debug
+    
     print("didFinishLaunchingWithOptions...")
     return true
   }
