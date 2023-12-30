beb14eedfc178ba06e195bfaa865e924f9edf880
Glenn Posadas <hello@glennvon.com>
2023-12-31 04:02:01 +0800

    attempt to force portrait.

--- BabySwipe/App/AppDelegate.swift
+++ BabySwipe/App/AppDelegate.swift
@@ -19,9 +19,20 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     FirebaseApp.configure()
     
     GADMobileAds.sharedInstance().start(completionHandler: nil)
-        
+    
     Purchases.logLevel = .debug
     Purchases.configure(withAPIKey: AppKeys.revCatAPIKey, appUserID: AppKeys.revCatAppId)
+    
+    
+    
+    if let rootViewController = UIApplication.shared.keyWindow?.rootViewController,
+       let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+      
+      rootViewController.setNeedsUpdateOfSupportedInterfaceOrientations()
+      
+      windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .portrait))
+    }
+    
     
     debugPrint("didFinishLaunchingWithOptions...")
     return true
